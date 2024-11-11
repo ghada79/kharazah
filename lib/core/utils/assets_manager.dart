9124a366b7c830f2895f8f96c505bb22d3fdacf5
MohamedDesign <m7medyt2017@gmail.com>
2024-11-11 13:16:43 +0200

    DONE HOMESCREEN AND LOGIN

--- lib/core/utils/assets_manager.dart
+++ lib/core/utils/assets_manager.dart
@@ -6,44 +6,18 @@ const String language = 'lib/config/language';
 
 abstract class ImageAssets {
   static const String cake = '${imagesPath}cake.png';
+  static const String cake2 = '${imagesPath}cake2.png';
 
 }
 
 abstract class IconAssets {
   static const String person = '${iconsPath}person.png';
   static const String home = '${iconsPath}home.png';
-  static const String setting = '${iconsPath}setting.png';
-  static const String ring = '${iconsPath}ring.png';
-  static const String checklist = '${iconsPath}Checklist.png';
-  static const String dollar = '${iconsPath}Dollar Currency Symbol.png';
-  static const String exam = '${iconsPath}Exam.png';
-  static const String examA = '${iconsPath}Exam A Plus.png';
-  static const String group = '${iconsPath}Group.png';
-  static const String id = '${iconsPath}ID.png';
-  static const String star = '${iconsPath}Star.png';
-  static const String table = '${iconsPath}Table.png';
-  static const String lock = '${iconsPath}Lock.png';
-  static const String email = '${iconsPath}Email.png';
+
 }
 
 abstract class SvgAssets {
-  static const String aPlus = '${svgPatch}APlus.svg';
-  static const String checklist = '${svgPatch}Checklist.svg';
-  static const String dollar = '${svgPatch}dollar.svg';
-  static const String email = '${svgPatch}email.svg';
-  static const String exam = '${svgPatch}Exam.svg';
-  static const String group = '${svgPatch}group.svg';
-  static const String id = '${svgPatch}ID.svg';
-  static const String table = '${svgPatch}Table.svg';
-  static const String notification = '${svgPatch}Notification.svg';
-  static const String profile = '${svgPatch}User.svg';
-  static const String home = '${svgPatch}Home.svg';
-  static const String settings = '${svgPatch}Gear.svg';
-  static const String warning = '${svgPatch}Warning.svg';
 }
 
 abstract class JsonAssets {
-  static const String warning = '${jsonPatch}warning.json';
-  static const String warning2 = '${jsonPatch}warning_updated.json';
-  static const String warning3 = '${jsonPatch}warning_circle_exclamation_modified.json';
 }
--- lib/features/home_screen/presentation/widgets/item_of_listview.dart
+++ lib/features/home_screen/presentation/widgets/item_of_listview.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:kharazah/core/components/custom_all_screens_appbar.dart';
 import 'package:kharazah/core/utils/assets_manager.dart';
 import 'package:kharazah/core/utils/color_mananger.dart';
+import 'package:kharazah/core/utils/styles_manager.dart';
 
 class ItemOfListview extends StatelessWidget {
   const ItemOfListview({Key? key}) : super(key: key);
@@ -10,14 +10,45 @@ class ItemOfListview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 150.w,
-      height: 40.h,
+      padding: EdgeInsets.all(8.w), // Adjust padding based on screen size
+      width: 150.w, // Width adapts to screen width
+      height: 200.h, // Adjust height for better aspect ratio on all devices
       decoration: BoxDecoration(
-        color: ColorManager.secondaryPrimary.withOpacity(0.5)
+        color: ColorManager.secondaryPrimary.withOpacity(0.2),
+        borderRadius: BorderRadius.all(Radius.circular(16.r)),
       ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start, // Aligns all text to start
         children: [
-          Image.network("assets/images/cake.png", height: 20, width:150.w,)
+          Expanded(
+            flex: 2,
+            child: Image.asset(
+              ImageAssets.cake2,
+              height: 100.h, // Image scales with screen height
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(height: 8.h), // Responsive space between elements
+          Expanded(
+            flex: 1,
+            child: Text(
+              'Chocolate Cake From AlAbd',
+              style: getMediumStyle(color: ColorManager.black, fontSize: 14.sp), // Font scales with screen size
+              textAlign: TextAlign.left, // Aligns text to the left
+            ),
+          ),
+          Text(
+            'Delicious and freshly baked',
+            style: getMediumStyle(color: ColorManager.black.withOpacity(0.7), fontSize: 12.sp),
+            textAlign: TextAlign.left, // Aligns text to the left
+          ),
+          Text(
+            '220 SAR',
+            style: getBoldStyle(color: ColorManager.primary, fontSize: 16.sp),
+            textAlign: TextAlign.left, // Aligns text to the left
+          ),
+
         ],
       ),
     );
